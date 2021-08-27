27774fc0fc3169dcff465928bd258332531422dd
Tarcísio de Pinho Silva Filho <89551990+TarcisioFilhox@users.noreply.github.com>
2021-08-27 20:34:51 -0300

    Create 5º.java

---
+++ 5º.java
@@ -0,0 +1,16 @@
+public class main {
+
+    	    public static void main(String[] args){
+    	       
+    	    	 for (int num1 = 1; num1 < 9; num1++){ 
+    	    		  System.out.println("\nTabuada de "+num1); 
+    	    		  
+    	    		  for (int num2 = 1; num2 <= 10; num2++){ 
+    	    		  System.out.print(num1 + " x " + num2 + " = " + num1 * num2 + " , ");
+    	    		  }
+    	    		
+    	    	 }
+    	    		 
+    	    }
+    	    
+    	}
